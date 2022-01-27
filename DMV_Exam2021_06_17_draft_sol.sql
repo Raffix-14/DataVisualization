7f22df83e4b3f7b56ede74ff351af6af86fb2ca7
Alessio Paone <32801591+Raffix-14@users.noreply.github.com>
2022-01-27 12:04:49 +0100

    a

--- DMV_Exam2021_06_17_draft_sol.sql
+++ DMV_Exam2021_06_17_draft_sol.sql
@@ -1,3 +1,27 @@
+SELECT CourierAgencyId, CourierAgencyName, City,
+    100 * SUM(#packages) / SUM(SUM(#packages)) OVER (PARTITION BY CourierAgencyId, Region) AS A,
+    SUM(total_weight) / SUM(#packages) AS B,
+    RANK() OVER(PARTITION BY CorporateGroup, City
+                ORDER BY SUM(#packages) DESC) AS C
+FROM COURIERAGENCY CA, LOCATION L, SHIPPINGS S
+WHERE S.CourierAgencyId = CA.CourierAgencyId
+AND L.LocationId = S.DepartureLocationId
+GROUP BY CourierAgencyId, CourierAgencyName, City, Region, CorporateGroup
+
+
+SELECT month, province,
+        SUM(#packages) / COUNT(DISTINCT TimeID) AS A,
+        SUM(SUM(total_weight)) OVER(PARTITION BY L1.province, L2.province, semester ORDER BY month ASC ROWS UNBOUNDED PRECEDING) AS B
+FROM TIME T, LOCATION L1, LOCATION L2, SHIPPINGS S
+WHERE L1.LocationId = S.DepartureLocationId
+AND L2.LocationId = S.ArrivalLocationId
+AND T.TimeID = S.TimeID
+GROUP BY month, L1.province, L2.province, semester
+
+
+
+
+
 12)
 db.cameras.find(
     {
